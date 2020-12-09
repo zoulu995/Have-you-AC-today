cd6187e382d16d5b6114d9f596249b84edf92958
TomZou <zouyuhang@pku.edu.cn>
2020-12-10 07:21:13 +0800

    20201210

---
+++ Acwing/acwing799.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100010;
+int n, arr[N];
+unordered_map<int, int> hashs;
+
+int main() {
+    cin >> n;
+    int res = 0;
+    for (int i = 0; i < n; i ++) cin >> arr[i];
+    for (int i = 0, j = 0; i < n; i ++) {
+        hashs[arr[i]] ++;
+        while (j <= i && hashs[arr[i]] > 1) hashs[arr[j++]] --;
+        if (j <= i) res = max(res, i - j + 1); 
+    }
+    cout << res << endl;
+    return 0;
+}
---
+++ Acwing/acwing803.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int N = 1e5 + 10;
+const int MIN = -2e9;  //最小值取错了！ 不是2e-9
+typedef pair<int, int> PII;
+vector<PII> segs, ans;
+int n;
+
+int merge(vector<PII>& segs) {
+    int st = MIN, ed = MIN;
+    for (auto seg : segs) {
+        if (ed < seg.first) {
+            if (st != MIN) {
+                ans.push_back({st, ed});
+            }
+            st = seg.first;
+            ed = seg.second;
+        }
+        else {
+            ed = max(seg.second, ed);
+        }
+    }
+    //cout << st << " " << ed << endl;
+    if (st != MIN) ans.push_back({st, ed});
+
+    return ans.size();
+}
+
+int main() {
+    cin >> n;
+    while (n --) {
+        int l, r; 
+        cin >> l >> r;
+        segs.push_back({l, r});
+    }
+    sort(segs.begin(), segs.end());
+    //for (auto seg : segs) cout << seg.first << " " << seg.second << endl;
+    cout << merge(segs) << endl;
+
+    return 0;
+}
