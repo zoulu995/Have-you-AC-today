7da2cdb3e94e7e35e9ff3dfe090ed267bc563e30
TomZou <zouyuhang@pku.edu.cn>
2020-12-23 09:17:54 +0800

    20201223

---
+++ Acwing/acwing154.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 300010;
+deque<int> q;
+int n, k;
+int arr[N];
+
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i ++) cin >> arr[i];
+    for (int i = 0; i < n; i ++) {
+        while (q.size() && i - q.front() >= k) q.pop_front();
+        while (q.size() && arr[q.back()] >= arr[i]) q.pop_back();
+        q.push_back(i);
+        if (i - k >= -1)
+            cout << arr[q.front()] << " ";
+        
+    }
+    cout << endl;
+    
+    for (int i = 0; i < n; i ++) {
+        while (q.size() && i - q.front() >= k) q.pop_front();
+        while (q.size() && arr[q.back()] <= arr[i]) q.pop_back();
+        q.push_back(i);
+        if (i - k + 1 >= 0)
+            cout << arr[q.front()] << " ";
+    }
+    return 0;
+}
---
+++ Acwing/acwing830.cpp
@@ -0,0 +1,21 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100010;
+int n;
+int arr[N];
+stack<int> stk;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i ++) {
+        cin >> arr[i];
+        while (stk.size() && arr[stk.top()] >= arr[i]) stk.pop();
+        if (stk.empty()) cout << "-1 ";
+        else cout << arr[stk.top()] << " ";
+        stk.push(i);
+    }
+    
+    
+    return 0;
+}
