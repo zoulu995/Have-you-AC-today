267e1bca98c8927479c2732665a3a0cd2cfb0b7f
TomZou <zouyuhang@pku.edu.cn>
2020-12-23 12:35:45 +0800

    20201223

---
+++ Acwing/acwing835.cpp
@@ -0,0 +1,37 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 100010;
+int son[N][26], cnt[N], idx;
+
+void insert(string &str) {
+    int p = 0;
+    for (int i = 0; str[i]; i ++) {
+        int u = str[i] - 'a';
+        if (!son[p][u]) son[p][u] = ++idx;
+        p = son[p][u];
+    }
+    cnt[p] ++;
+}
+
+int query(string &str) {
+   int p = 0;
+   for (int i = 0; str[i]; i ++) {
+       int u = str[i] - 'a';
+       if (!son[p][u]) return 0;
+       p = son[p][u];
+   }
+   return cnt[p];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    while (n --) {
+        string op, str;
+        cin >> op >> str;
+        if (op == "I") insert(str);
+        else cout << query(str) << endl;
+    }
+    return 0;
+}
---
+++ Acwing/acwing836.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100010;
+int n, m;
+int p[N];
+
+int find(int x) {
+    if (x != p[x]) p[x] = find(p[x]);
+    return p[x];
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i ++) p[i] = i;
+    while (m --) {
+        string op;
+        int a, b;
+        cin >> op >> a >> b;
+        if (op == "M") {
+            if (find(a) != find(b)) p[find(a)] = find(b);
+        }
+        else {
+            if (find(a) == find(b)) cout << "Yes" << endl;
+            else cout << "No" << endl;
+        }
+    }
+    return 0;
+}
